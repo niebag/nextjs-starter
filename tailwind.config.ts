875828f16590d77b33730566c02cd5dd68e7177a
Niels Baggerman <niels.baggerman@notive.nl>
2024-01-24 21:50:21 +0100

    style: format

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,11 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  /**
- * Configure the paths to all of your HTML templates, JS components, and any other files that contain Tailwind class names.
- * @see https://tailwindcss.com/docs/content-configuration
- */
-  content: ['./src/**/*.tsx'],
+	/**
+	 * Configure the paths to all of your HTML templates, JS components, and any other files that contain Tailwind class names.
+	 * @see https://tailwindcss.com/docs/content-configuration
+	 */
+	content: ['./src/**/*.tsx']
 };
 
 export default config;
