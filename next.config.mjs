cab2425eeb879a4c75498ae0562aed7322391105
Niels Baggerman <niels.baggerman@notive.nl>
2024-01-24 22:06:37 +0100

    feat: added environment vars validation

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,10 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+await import("./src/lib/env/client.js");
+await import("./src/lib/env/server.js");
+
 /** @type {import('next').NextConfig} */
 const config = {};
 
