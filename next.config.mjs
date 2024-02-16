57196d6ea0c9c19935da4e8e2c078d514dd9bdca
Niels Baggerman <niels.baggerman@notive.nl>
2024-02-16 12:46:03 +0100

    build: concat env files into single file

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/lib/env/client.js");
-await import("./src/lib/env/server.js");
+await import("./src/env.js");
 
 /** @type {import('next').NextConfig} */
 const config = {};
