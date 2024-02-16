40be69ff2567a8cb8751a1ef1d14271f072cadbb
Niels Baggerman <niels.baggerman@notive.nl>
2024-02-16 12:45:33 +0100

    chore: re-add create-next-app homepage

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,19 @@ const config: Config = {
 	 * Configure the paths to all of your HTML templates, JS components, and any other files that contain Tailwind class names.
 	 * @see https://tailwindcss.com/docs/content-configuration
 	 */
-	content: ['./src/**/*.tsx']
+	content: ['./src/**/*.tsx'],
+
+	/**
+	 * Define your project’s color palette, type scale, fonts, breakpoints, border radius values, and more.
+	 */
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+			}
+		}
+	}
 };
 
 export default config;
