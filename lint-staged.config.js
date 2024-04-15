3ca3074a78abb37a04574fa979171df7d4bca35e
Niels Baggerman <niels.baggerman@notive.nl>
2024-04-15 11:58:15 +0200

    fix: missing path for bulk format command

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -12,7 +12,7 @@ function lint(filenames) {
 /** @type {import("lint-staged").ConfigFn} */
 function prettier(filenames) {
 	// If there are more than 10 staged files, run prettier over the whole project
-	if (filenames.length > 10) return 'prettier --write';
+	if (filenames.length > 10) return 'prettier --write .';
 
 	// Otherwise, run prettier over each file individually
 	return `prettier --write ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`;
