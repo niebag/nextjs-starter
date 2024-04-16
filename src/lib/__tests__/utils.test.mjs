1b0acccdeef7467e15cff3184691a37c218f954c
Niels Baggerman <niels.baggerman@notive.nl>
2024-04-17 00:49:48 +0200

    test: fix expectation

--- src/lib/__tests__/utils.test.mjs
+++ src/lib/__tests__/utils.test.mjs
@@ -4,6 +4,6 @@ describe('cn', () => {
 	it('merges tailwind classes', () => {
 		const classNames = cn('bg-red hover:bg-dark-red px-2 py-1', 'bg-[#b91c1c] p-3');
 
-		expect(classNames).toBe('hover:bg-dark-red p-3 bg-[#b91c1c]');
+		expect(classNames).toEqual(expect.stringContaining('hover:bg-dark-red p-3 bg-[#b91c1c]'));
 	});
 });
