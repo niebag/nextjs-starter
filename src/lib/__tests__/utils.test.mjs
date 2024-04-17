b0ff22883f129a75a98ca7555fb479cddd3b0ef9
Niels Baggerman <niels.baggerman@notive.nl>
2024-04-17 08:58:17 +0200

    test: fix expectation

--- src/lib/__tests__/utils.test.mjs
+++ src/lib/__tests__/utils.test.mjs
@@ -1,9 +1,11 @@
 import { cn } from '~/lib/utils';
 
 describe('cn', () => {
-	it('merges tailwind classes', () => {
+	it('merges tailwind class names', () => {
 		const classNames = cn('bg-red hover:bg-dark-red px-2 py-1', 'bg-[#b91c1c] p-3');
+		const expectedClasses = ['hover:bg-dark-red', 'p-3', 'bg-[#b91c1c]'];
 
-		expect(classNames).toEqual(expect.stringContaining('hover:bg-dark-red p-3 bg-[#b91c1c]'));
+		expect(classNames.split(' ').sort()).toEqual(expectedClasses.sort());
+		expect(classNames.split(' ').length).toBe(expectedClasses.length);
 	});
 });
