016cb2cd87ae13de0cf4918342435e908f13a05d
Niels Baggerman <niels.baggerman@notive.nl>
2024-04-17 11:24:46 +0200

    feat: added plop

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export function toPascalCase(string: string) {
+	return string
+		.replace(/(\w)(\w*)/g, function (g0: string, g1: string, g2: string) {
+			return g1.toUpperCase() + g2.toLowerCase();
+		})
+		.replace(/\s+/g, '');
+}
